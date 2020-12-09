c1e50db3ac9b13de454d85a03e8ee2a32c94c188
Jorge Lopez <jorgekl.1994@gmail.com>
2020-12-08 22:19:49 -0600

    Primero

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const indexRoutes = require('./routes/index');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('', indexRoutes);
+
+app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
