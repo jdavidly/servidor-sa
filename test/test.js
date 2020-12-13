08937d0b34533add6f494f0ed8938821833582ed
ErickTejaxun <erickteja@gmail.com>
2020-12-13 02:59:52 -0600

    Add first test unit using mocha

---
+++ test/test.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+const { json } = require('body-parser');
+var expect = require('expect');
+var Request = require("request");
+describe('Pruebas unitarias sobre el servidor de backend', ()=>
+{
+    it('Prueba de login. El intento de logeo debe ser exitoso', (done)=>
+    {
+        Request.post(            
+        {
+            url:"http://localhost:3000/user/login",
+            form:
+            {
+                email: 'jorge@gmail.com',
+                password:'123'
+            },
+        },
+        function(error, response, body)
+        {
+            if(!error)
+            {
+                expect(JSON.parse(response.body)['auth']).toBe(true);                                  
+                done();
+            }
+            else
+            {
+                console.log(error);
+            }
+        }        
+        );
+    });
+
+    it('Prueba de login. El intento de logeo debe fallar', (done)=>
+    {
+        Request.post(            
+        {
+            url:"http://localhost:3000/user/login",
+            form:
+            {
+                email: 'jorge@gmail.com',
+                password:'1234'
+            },
+        },
+        function(error, response, body)
+        {
+            if(!error)
+            {
+                expect(JSON.parse(response.body)['auth']).toBe(false);                                  
+                done();
+            }
+            else
+            {
+                console.log(error);
+            }
+        }        
+        );
+    });
+
+});
