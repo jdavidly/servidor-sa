d111aa4127dbb4d6bc71141d13373d976af35810
ErickTejaxun <erickteja@gmail.com>
2020-12-20 17:31:23 -0600

    New version

--- microservicio-usuario/routes/index.js
+++ microservicio-usuario/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Nueva versión con GKE-Ansible");
+    res.send("Nueva versión con GKE-Ansible 2020");
 });
 
 module.exports = router;
